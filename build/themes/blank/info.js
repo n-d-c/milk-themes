1d18390fafcb0cc1836a44cfeb4881091edf4d81
rndm-user <accounts@random-user.com>
2021-05-12 17:21:02 -0700

    update: fixing updates.

---
+++ build/themes/blank/info.js
@@ -0,0 +1,14 @@
+/* #### THEME #### */
+const theme = {
+	name: 'Blank',
+	slug: 'blank',
+	version: '0.0.02',
+	date: '2021-05-12',
+	url: 'https://milkjs.com/themes/blank',
+	author: 'Random-User (DevLove)',
+	tagline: 'Example theme for Milk.js.',
+	excerpt: 'Please feel free to take this theme and copy it to start your own if you desire.',
+	darkmode: true,
+	prismjs: true
+};
+export { theme };
