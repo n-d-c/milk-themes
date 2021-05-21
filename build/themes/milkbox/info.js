78663c470987895e5115dba7fed71139252248af
JoshuaJarman <josh@nelsondesigncollective.com>
2021-05-20 20:24:48 -0700

    updated: update process.

---
+++ build/themes/milkbox/info.js
@@ -0,0 +1,14 @@
+/* #### THEME #### */
+const theme = {
+	name: 'MilkBox',
+	slug: 'milkbox',
+	version: '0.0.08',
+	date: '2021-04-19',
+	url: 'https://milkjs.com/themes/milkbox',
+	author: 'Random-User (DevLove)',
+	tagline: 'Example theme for Milk.js.',
+	excerpt: 'Please feel free to take this theme and copy it to start your own if you desire.',
+	darkmode: true,
+	prismjs: true
+};
+export { theme };
